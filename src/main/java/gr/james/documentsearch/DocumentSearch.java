593ad5cd608e80a1ba4304189efe3eaa6d6e703e
Giorgos Stamatelatos <gstamatelat@gmail.com>
2017-12-28 19:38:56 +0200

    Enrich javadoc

--- src/main/java/gr/james/documentsearch/DocumentSearch.java
+++ src/main/java/gr/james/documentsearch/DocumentSearch.java
@@ -4,15 +4,24 @@ import java.util.*;
 
 public class DocumentSearch {
     /**
-     * Returns the smallest interval in which query appear in words in the order given. Returns -1 if no interval found.
-     * This method uses extra space proportional to words.length + query.length. T must comply with hashCode/equals
-     * contract.
+     * Returns the smallest interval in which {@code query} appear in {@code words} in the order given.
+     * <p>
+     * Returns -1 if no interval found.
+     * <p>
+     * This method uses extra space proportional to {@code words.length + query.length}. {@code T} must comply with
+     * hashCode/equals contract.
+     *
+     * @param words the collection of items to search in
+     * @param query the collection of items to search for
+     * @param <T>   the type of items
+     * @return the smallest interval in which {@code query} appear in {@code words} in the order given or {@code -1} if
+     * no interval found
      */
     public static <T> int search(Iterator<T> words, Iterator<T> query) {
         final TreeSet<Integer> emptyTreeSet = new TreeSet<>();
 
         /* Basic data structure, needs to be a List with fast get, add and next */
-        List<TreeSet<Integer>> sets = new ArrayList<>();
+        final List<TreeSet<Integer>> sets = new ArrayList<>();
 
         /* Terminate early (also avoid some exceptions) */
         if (!words.hasNext() || !query.hasNext()) {
@@ -20,7 +29,7 @@ public class DocumentSearch {
         }
 
         /* Inverted word index */
-        Map<T, TreeSet<Integer>> invertedWordMap = new HashMap<>();
+        final Map<T, TreeSet<Integer>> invertedWordMap = new HashMap<>();
         for (int i = 0; words.hasNext(); ) {
             invertedWordMap.computeIfAbsent(words.next(), w -> new TreeSet<>()).add(i++);
         }
